80884d221c787b8f2c3fd9b6e0fe377dd3eb9e86
mariethndev <contactmariecode@gmail.com>
2025-09-30 16:42:24 +0200

    Mise à jour du porfolio de Marie dans le dossier, suppression de hotel.com dans l'exercice

---
+++ Portfolio_Marie/script.js
@@ -0,0 +1,11 @@
+function ouvrirCV() {
+  window.open("cv.pdf", "_blank");
+}
+
+function ouvrirProjet() {
+  window.open("zoo.pdf", "_blank");
+}
+
+function projetChat() {
+  window.open("chat.pdf", "_blank");
+}
